12b9ec898948acc2d560922efe26b82942a1b018
kishan shah <kishan.shah365@gmail.com>
2022-11-22 18:04:38 +0545

    modified Desitaniton catagory

--- Travel.CoreLayer/Src/Services/DestinationCategory/DestinationCategoryService.cs
+++ Travel.CoreLayer/Src/Services/DestinationCategory/DestinationCategoryService.cs
@@ -3,10 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Transactions;
+using Travel.CoreLayer.DTO.DestinationCategoryDto;
+using Travel.CoreLayer.Exception;
+using Travel.CoreLayer.Helper;
+using Travel.CoreLayer.Repository;
 
-namespace Travel.CoreLayer.Src.Services.DestinationCategory
+namespace Travel.CoreLayer.Services.DestinationCategory
 {
-    internal class DestinationCategoryService
+    public class DestinationCategoryService:DestinationCategoryServiceInterface
     {
+        private readonly DestinationCategoryRepositoryInterface DestinationCategoryRepository;
+        public DestinationCategoryService(DestinationCategoryRepositoryInterface _destinationCategoryRepository) {
+        DestinationCategoryRepository =_destinationCategoryRepository;
+        }
+
+        public async Task Activate(long Id)
+        {
+            using TransactionScope scope = TransactionScopeHelper.GetInstance();
+            var vehicleCategory = await DestinationCategoryRepository.GetById(Id).ConfigureAwait(false) ?? throw new DestinationCategoryNotFoundException();
+            vehicleCategory.Activate();
+            scope.Complete();
+        }
+
+        public Task Create(DestinationCategoryInsertDto dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task Deactivate(long Id)
+        {
+            using TransactionScope scope = TransactionScopeHelper.GetInstance();
+            var destinationCategory = await DestinationCategoryRepository.GetById(Id).ConfigureAwait(false) ?? throw new VehicleCategoryNotFoundException();
+        
+            destinationCategory.Deactivate();
+            scope.Complete();
+        }
+
+        public Task Update(DestinationCategoryUpdateDto dto)
+        {
+            throw new NotImplementedException();
+        }
+
+       
     }
 }
